e51e80984baba3e81c0a2e22389b8816ac60c6b6
ThomasCross257 <thomascross257@gmail.com>
2023-10-04 16:43:21 -0700

    Give me a break, it's 2am. But animations work now.
    Also I converted this to blueprint but feel free to make C++ classes later on if you want.

--- NoMand_Knight/Intermediate/Build/Win64/NoMand_KnightEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.37.32822
-#include "D:/Program Files (x86)/Users/Mike Rowe Soft/Documents/CSE 4050/CSE4400-GameDesignProject/NoMand_Knight/Intermediate/Build/Win64/NoMand_KnightEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- NoMand_Knight/Source/NoMand_Knight/NoMand_Knight.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NoMand_Knight.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoMand_Knight, "NoMand_Knight" );
